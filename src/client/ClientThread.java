f4d24783d7cac6485d0446be29096d023019dbbd
error <bingyang038@gmail.com>
2021-03-29 20:24:41 +0800

    organize the switch{RRQ,WRQ,DATA,ACK,ERROR} for server

--- src/client/ClientThread.java
+++ src/client/ClientThread.java
@@ -1,6 +1,5 @@
 package client;
 
-import tftp.TFTFConstants;
 import tftp.TFTP;
 
 import java.io.*;
@@ -9,7 +8,10 @@ import java.net.*;
 
 public class ClientThread extends TFTP {
 	private ClientGUI gui;
-	private DatagramPacket pkt = null;
+	/**
+	 * {InetAddress address},{DatagramPacket dp}
+	 * and {DatagramSocket ds} from extends TFTP
+	 */
 	public ClientThread(){}//constructor
 	public ClientThread(ClientGUI _gui){
 		this.gui = _gui;
@@ -19,31 +21,37 @@ public class ClientThread extends TFTP {
 	public void run() {
 		System.out.println("ClientConnect");
 		try {
+			/*
+			send data to server
+			 */
 			address = InetAddress.getByName(gui.getTfServer().getText());	//get the localhost
 			byte[] a = "hello".getBytes();
 			DATAPacket(address,1,a,SERVER_PORT);
 
-
-			//ready to read from server
-			byte[] data = new byte[1024];
-			dp = new DatagramPacket(data,data.length);	//ready the packet
-			ds = new DatagramSocket(CLIENT_PORT);				//packet ready the receive the port
-			ds.receive(dp);
-			ByteArrayInputStream ab = new ByteArrayInputStream(data);
-			DataInputStream dis = new DataInputStream(ab);
-			String content = "";
-			byte b;
-			while ((b = dis.readByte()) > 0) {
-				content += (char) b;
-				System.out.println(b);
-			}
-			log(content);
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
+			ReadFromServer();	//ready to read from server
 		} catch (IOException e) {
 			e.printStackTrace();
-		}
+		}//try..catch
 	}//run
+
+	/*
+	ready to read from server
+	 */
+	private void ReadFromServer() throws IOException {
+		byte[] data = new byte[1024];
+		dp = new DatagramPacket(data,data.length);	//ready the packet
+		ds = new DatagramSocket(CLIENT_PORT);				//packet ready the receive the port
+		ds.receive(dp);					//will stop here until read the data
+		ByteArrayInputStream ab = new ByteArrayInputStream(data);	// data ↑
+		DataInputStream dis = new DataInputStream(ab);
+		String content = "";
+		byte b;
+		while ((b = dis.readByte()) > 0) {
+			content += (char) b;
+			System.out.println(b);
+		}
+		log(content);
+	}//read from server
 
 	private void log(String msg){
 		gui.getTaLog().appendText(msg + "\n");
--- src/client/ClientUpload.java
+++ src/client/ClientUpload.java
@@ -4,13 +4,16 @@ import tftp.TFTP;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.InetAddress;
-import java.net.UnknownHostException;
 
 public class ClientUpload extends TFTP {
 	private ClientGUI gui;
+	/**
+	 * {InetAddress address},{FileInputStream fis}
+	 * and {File f} from extends TFTP
+	 */
+
 	public ClientUpload(){}//constructor
 	public ClientUpload(ClientGUI _gui){
 		this.gui = _gui;
@@ -18,29 +21,25 @@ public class ClientUpload extends TFTP {
 
 	@Override
 	public void run() {
-		System.out.println("click upload");
+		System.out.println("click upload");	//click upload
 		int blockNum = 0;
 		try {
 			address = InetAddress.getByName(gui.getTfServer().getText());	//get the localhost
 
 			f = new File("Lincoln.txt");		//use file here,
-			System.out.println(f.getCanonicalFile());
+//			System.out.println(f.getCanonicalFile());
 			fis = new FileInputStream(f);
 			int ba = 0;
 			byte[] a = new byte[512];
 			int len = 0;
 			while((ba=fis.read(a)) > 0){
-				System.out.println(ba);		//print out total data how many time that fill the byte[512]
+//				System.out.println(ba);		//print out total data how many time that fill the byte[512]
 				DATAPacket(address,blockNum,a,SERVER_PORT);	//just data, send message
 				a = new byte[512];
 				blockNum++;
 			}//while
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
-		}
+		}//try..catch
 	}//run
 }//class
